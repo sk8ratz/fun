d6519561e09974fdb724d75416064acf8feeeec2
sk8ratz <ratzbeskatin@gmail.com>
2023-04-17 14:22:56 -0500

    Initial commit

---
+++ image-grid.js
@@ -0,0 +1,64 @@
+const cid = "QmeL5MQmsdejM1caUJRB9gRHXAduZQ6Zf7ajtA19GN35tP";
+const urlFormat = "https://ipfs.io/ipfs/" + cid + "/";
+const container = document.querySelector(".container");
+const numImagesPerBatch = 40;
+let startIndex = 0;
+let loadedImages = new Set();
+let imageCache = {};
+
+// Create an IntersectionObserver instance
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && !loadedImages.has(entry.target.dataset.index)) {
+      // Load the image and store it in the cache
+      const img = entry.target;
+      img.src = urlFormat + (parseInt(img.dataset.index) + 1) + ".png";
+      img.loading = "lazy"; // add lazy loading attribute
+      loadedImages.add(parseInt(img.dataset.index));
+      imageCache[parseInt(img.dataset.index)] = img;
+      // Disable right-click context menu on the image
+      img.addEventListener('contextmenu', e => {
+        e.preventDefault();});
+    }
+  });
+});
+
+// Load the initial batch of images
+for (let i = startIndex; i < startIndex + numImagesPerBatch; i++) {
+  const img = document.createElement("img");
+  img.dataset.index = i;
+  img.loading = "lazy"; // add lazy loading attribute
+  observer.observe(img); // observe the image element
+  container.appendChild(img);
+}
+
+// Update the start index for the next batch
+startIndex += numImagesPerBatch;
+
+// Listen for the scroll event and load more images as needed
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY || window.pageYOffset;
+  const threshold = document.documentElement.scrollHeight - window.innerHeight;
+
+  if (scrollY >= threshold) {
+    // Load the next batch of images
+    for (let i = startIndex; i < startIndex + numImagesPerBatch; i++) {
+      if (!loadedImages.has(i)) {
+        const img = document.createElement("img");
+        img.dataset.index = i;
+        img.loading = "lazy"; // Add lazy loading attribute
+        observer.observe(img); // observe the image element
+        container.appendChild(img);
+      }
+    }
+
+    // Update the start index for the next batch
+    startIndex += numImagesPerBatch;
+  }
+});
+
+// Restore images from cache on page reload
+window.addEventListener("load", () => {
+  const cachedImages = Object.values(imageCache);
+  container.append(...cachedImages);
+});
---
+++ main.js
@@ -0,0 +1,45 @@
+setTimeout(function() {
+    document.getElementById("continue-msg").innerText = "PRESS SK8 RATZ TO CONTINUE";
+  }, 3000); // 3000 milliseconds = 3 seconds
+
+const continueMsg = document.getElementById('continue-msg');
+
+// Wait for the text to appear before starting the animations
+setTimeout(() => {
+  continueMsg.classList.add('show');
+}, 3000);
+
+setInterval(() => {
+  continueMsg.classList.toggle('blue');
+  continueMsg.classList.toggle('white');
+}, 3000);
+
+
+// Get the close button and navbar elements
+const closeButton = document.querySelector('.close-btn');
+const navbar = document.querySelector('.navbar');
+
+// Show/hide the navbar when the logo is clicked
+function showNavbar() {
+  navbar.classList.toggle('show');
+  if (navbar.classList.contains('show')) {
+    closeButton.style.display = 'block';
+  } else {
+    closeButton.style.display = 'none';
+  }
+}
+
+// Hide the navbar and close button when the close button is clicked
+closeButton.addEventListener('click', function(event) {
+  event.stopPropagation();
+  navbar.classList.remove('show');
+  closeButton.style.display = 'none';
+});
+
+// Hide the navbar and close button when the user clicks outside of the navbar
+navbar.addEventListener('click', function(event) {
+  if (event.target === navbar) {
+    navbar.classList.remove('show');
+    closeButton.style.display = 'none';
+  }
+});
